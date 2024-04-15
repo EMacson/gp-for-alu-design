41075fea9974599332259397f9f66558b8f5418d
ethan m <48036998+EMacson@users.noreply.github.com>
2024-04-15 13:43:17 -0600

    created system structure

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    # create run dir
+
+    # create first generation
+
+    # run selector
+
+    # teriminate?
+
+    # cross over
+
+    # create new generation
+
+    # mutate
+
+    # loop back
+    pass
+
+if __name__ == "__main__":
+    main()
