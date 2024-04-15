c54088bb79eadd4c0852cbf8f36e6bfe50044d36
ethan m <48036998+EMacson@users.noreply.github.com>
2024-04-15 13:51:48 -0600

    main.py creates run directories for storing circuits

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-
+import os
+from datetime import datetime
 
 def main():
     # create run dir
+    # might need to create gitignore for new circuit directories
+    main_dir = os.path.dirname(os.path.abspath(__file__))
+    circuit_dir = os.path.join(main_dir, "circuits")
+    current_time = datetime.now()
+    current_time_str = current_time.strftime("%Y-%m-%d_%H-%M-%S")
+    subdirectory = current_time_str
+    subdirectory_path = os.path.join(circuit_dir, subdirectory)
+    os.makedirs(subdirectory_path)
 
     # create first generation
 
